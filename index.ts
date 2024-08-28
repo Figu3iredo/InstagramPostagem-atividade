ff561c195dfc38c2f61ab277d1c6c6d8aae48040
Lauro Viana <laurogold@gmail.com>
2024-08-28 00:10:11 -0300

    primeiro commit

---
+++ index.ts
@@ -0,0 +1,33 @@
+class Post {
+   private _userName: string; // Atributo privado do tipo string
+   private _titulo: string;
+   private _dataPostagem: Date;
+   private _numeroCurtidas: number;
+ 
+   constructor(userName: string, titulo: string) {
+     this._userName = userName;
+     this._titulo = titulo;
+     this._dataPostagem = new Date();
+     this._numeroCurtidas = 0;
+   }
+ 
+   get userName() {
+     return this._userName.toLocaleUpperCase();
+   }
+ 
+   set userName(userName: string) {
+     this._userName = userName;
+   }
+ 
+   incrementarCurtidas() {
+     this._numeroCurtidas += 1;
+   }
+ }
+ 
+ const post1 = new Post("augustocesar", "Foto 1");
+ 
+ console.log(post1.userName);
+ post1.userName = "Maria";
+ console.log(post1.userName);
+ 
+ post1.incrementarCurtidas();
