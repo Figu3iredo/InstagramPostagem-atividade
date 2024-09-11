363fd70d4184d68bb0e307b31e8d8daeb8630c19
Lauro Viana <laurogold@gmail.com>
2024-09-10 22:28:29 -0300

    segundo commit

--- index.ts
+++ index.ts
@@ -1,33 +1,110 @@
+import { v4 as randomUUID } from "uuid";
+import { faker } from "@faker-js/faker";
+
 class Post {
-   private _userName: string; // Atributo privado do tipo string
-   private _titulo: string;
-   private _dataPostagem: Date;
-   private _numeroCurtidas: number;
- 
-   constructor(userName: string, titulo: string) {
-     this._userName = userName;
-     this._titulo = titulo;
-     this._dataPostagem = new Date();
-     this._numeroCurtidas = 0;
-   }
- 
-   get userName() {
-     return this._userName.toLocaleUpperCase();
-   }
- 
-   set userName(userName: string) {
-     this._userName = userName;
-   }
- 
-   incrementarCurtidas() {
-     this._numeroCurtidas += 1;
-   }
- }
- 
- const post1 = new Post("augustocesar", "Foto 1");
- 
- console.log(post1.userName);
- post1.userName = "Maria";
- console.log(post1.userName);
- 
- post1.incrementarCurtidas();
+  private _id: string = randomUUID();
+  private _userName: string;
+  private _avatarUrl: string;
+  private _imageUrl: string;
+  private _isLiked: boolean = false;
+  private _description: string;
+  private _createdAt: Date = new Date();
+  private _numberOfLikes: number = 0;
+
+  constructor(
+    userName: string,
+    avatarUrl: string,
+    imageUrl: string,
+    description: string
+  ) {
+    this._userName = userName.toLowerCase();
+    this._avatarUrl = avatarUrl;
+    this._imageUrl = imageUrl;
+    this._description = description;
+  }
+
+  like() {
+    this._isLiked = !this._isLiked;
+
+    // Incrementa o número de likes
+    if (this._isLiked) {
+      this._numberOfLikes += 1;
+    } else {
+      // Descrementa o número de likes
+      this._numberOfLikes -= 1;
+    }
+
+    const postContainer = document.getElementById(this._id);
+    const btnLike = postContainer?.querySelector("#btn-like");
+
+    if (!btnLike) return;
+
+    btnLike.innerHTML = String(this._isLiked);
+    // console.log(btnLike);
+  }
+
+  toHTML() {
+    const postContainer = document.createElement("div");
+    postContainer.className = "post-container";
+    postContainer.id = this._id;
+
+    const postHeader = `
+      <div class="post-header">
+        <div>
+          <img title="Avatar image"
+            src=${this._avatarUrl}>
+        </div>
+        <span>${this._userName}</span>
+      </div>
+    `;
+
+    const postImage = `
+     <div class="post-image">
+        <img title="Post image"
+          src=${this._imageUrl}>
+      </div>
+    `;
+
+    const postIcons = `
+      <div class="post-icons">
+        <div>
+          <div id="btn-like">
+            <i class="fa fa-heart-o"></i>
+          </div>
+
+          <div>
+            <i class="fa fa-comment-o"></i>
+          </div>
+
+          <div>
+            <i class="fa fa-paper-plane-o"></i>
+          </div>
+        </div>
+
+        <i class="fa fa-bookmark-o"></i>
+      </div>
+    `;
+
+    postContainer.innerHTML = postHeader;
+    postContainer.innerHTML += postImage;
+    postContainer.innerHTML += postIcons;
+
+    const btnLike = postContainer.querySelector("#btn-like");
+    btnLike?.addEventListener("click", () => this.like());
+
+    document.body.appendChild(postContainer);
+  }
+}
+
+const posts: Post[] = [];
+
+for (let index = 0; index < 15; index++) {
+  const userName = faker.person.firstName();
+  const avatarUrl = faker.image.avatarGitHub();
+  const imageUrl = faker.image.urlLoremFlickr();
+  const description = faker.lorem.paragraph();
+
+  const post = new Post(userName, avatarUrl, imageUrl, description);
+
+  post.toHTML();
+}
